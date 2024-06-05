19c4016ac5e0a98c7ba31d037df040cfb744b34e
Daniil Veremchuk <daniel@verydanny.com>
2024-06-05 11:49:48 -0400

    try cloudflare-adjacent worker

--- hooks/hello-world/src/main.js
+++ hooks/hello-world/src/main.js
@@ -1,34 +1,32 @@
 import { Client } from 'node-appwrite';
-import { serve } from '@hono/node-server'
-
-import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
+
 app.get('/', (c) => c.text('Hello Node.js!'))
 
-serve(app)
+export default app
 
 // This is your Appwrite function
 // It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+// export default async ({ req, res, log, error }) => {
+//   // Why not try the Appwrite SDK?
+//   //
+//   // const client = new Client()
+//   //    .setEndpoint('https://cloud.appwrite.io/v1')
+//   //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+//   //    .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+//   // You can log messages to the console
+//   log('Hello, Logs!');
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+//   // If something goes wrong, log an error
+//   error('Hello, Errors!');
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
-};
+//   // The `req` object contains the request data
+//   if (req.method === 'GET') {
+//     // Send a response with the res object helpers
+//     // `res.send()` dispatches a string back to the client
+//     return res.send('Hello, World!');
+//   }
+// };
