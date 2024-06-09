cc5b31b1125156b2bfb6328ed959c29e7ba2b8fe
Daniil Veremchuk <daniel@verydanny.com>
2024-06-09 15:16:31 -0400

    minimal reproduction

--- hooks/hello-world/src/main.js
+++ hooks/hello-world/src/main.js
@@ -1,11 +1,11 @@
 /**
  * Using node-21.0 open-runtime
  */
-import { fileURLToPath } from 'node:url'
-import { html, raw } from 'hono/html'
-import * as path from 'node:path'
-import * as fs from 'node:fs'
-import { Readable, Stream } from 'node:stream'
+// import { fileURLToPath } from 'node:url'
+import { html } from 'hono/html'
+// import * as path from 'node:path'
+// import * as fs from 'node:fs'
+import { Readable } from 'node:stream'
 
 import { Hono } from 'hono'
 import { serveStatic } from './serveStatic.mjs'
@@ -14,7 +14,6 @@ import { getRequestListener } from './getRequestListener.mjs'
 const app = new Hono()
 
 app.use('/static/*', serveStatic({ root: 'src/function' }))
-
 app.get('/', (c) => c.html('Hello open-runtime!'))
 app.get('/some/other/route', (c) => c.html(
   html`<html>
@@ -23,15 +22,9 @@ app.get('/some/other/route', (c) => c.html(
 ))
 
 export default async ({ req, res, log, error }) => {
-  const requestListener = getRequestListener(app.fetch, {
-    overrideGlobalObjects: true
-  })
-
-  const initRequestListener = requestListener(error)
   const body = req?.method === 'GET' || req?.method === 'HEAD' ? undefined : req.body
 
   try {
-    // const response = await initRequestListener(req, res)
     const request = new Request(
       new URL(req.url),
       {
@@ -51,35 +44,4 @@ export default async ({ req, res, log, error }) => {
   } catch (e) {
     error(e)
   }
-
-  return res.json(
-    {
-      hello: 'world',
-    },
-    200
-  )
 }
-
-// This is your Appwrite function
-// It's executed each time we get a request
-// export default async ({ req, res, log, error }) => {
-//   // Why not try the Appwrite SDK?
-//   //
-//   // const client = new Client()
-//   //    .setEndpoint('https://cloud.appwrite.io/v1')
-//   //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-//   //    .setKey(process.env.APPWRITE_API_KEY);
-
-//   // You can log messages to the console
-//   log('Hello, Logs!');
-
-//   // If something goes wrong, log an error
-//   error('Hello, Errors!');
-
-//   // The `req` object contains the request data
-//   if (req.method === 'GET') {
-//     // Send a response with the res object helpers
-//     // `res.send()` dispatches a string back to the client
-//     return res.send('Hello, World!');
-//   }
-// };
