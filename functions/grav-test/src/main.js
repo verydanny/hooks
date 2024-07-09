1544e22f4ddb95a585306f67b1ff4a9964159e77
Daniil Veremchuk <daniel@verydanny.com>
2024-07-08 20:13:01 -0400

    simplify

--- functions/grav-test/src/main.js
+++ functions/grav-test/src/main.js
@@ -1,11 +1,6 @@
 import { Hono } from "hono"
 import { serve } from "@gravlabs/appwrite-hono-adapter-node"
 import { serveStatic } from "@gravlabs/appwrite-hono-adapter-node/serveStatic"
-import { resolve } from 'node:path'
-import { existsSync } from "node:fs"
-
-const openRuntimeRoot = 'src/function'
-const isOpenRuntimes = existsSync(resolve(process.cwd(), openRuntimeRoot))
 
 const app = new Hono()
 
@@ -21,8 +16,4 @@ app.get("/", (context) =>
     `),
 )
 
-export default (context) => {
-  context.log(context?.res?.binary)
-
-  return serve(app)(context)
-}
+export default serve(app)
