e471d9cd0f058cc5da80d97ddb502e129fe5a394
Daniil Veremchuk <daniel@verydanny.com>
2024-07-08 23:47:50 -0400

    try with my library

--- astro/main.js
+++ astro/main.js
@@ -2,42 +2,15 @@
  * Using node-21.0 open-runtime
  */
 import { handler as ssrHandler } from './dist/server/entry.mjs'
-import { Readable } from 'node:stream'
-import { getRequestListener } from './getRequestListener.mjs'
 
 import { Hono } from 'hono'
-import { serveStatic } from './serveStatic.mjs'
+import { serve } from "@gravlabs/appwrite-hono-adapter-node"
+import { serveStatic } from "@gravlabs/appwrite-hono-adapter-node/serveStatic"
 
 const app = new Hono()
 
 // Static files work perfectly
-app.use('/*', serveStatic({ root: 'src/function/dist/client' }))
+app.use('/*', serveStatic({ root: './dist/client' }))
 app.use(ssrHandler)
 
-const initListener = getRequestListener(app.fetch, {
-  overrideGlobalObjects: true
-})
-
-export default async ({ req, res, log, error }) => {
-  const listener = initListener(error)
-
-  try {
-    const response = await listener(req, res)
-    const blob = await response.blob()
-
-    let headers = {}
-    for (const [key, value] of response.headers.entries()) {
-      headers[key] = value
-    }
-
-    // This is only needed on Appwrite, if this isn't included
-    // then text and json-based routes will loop forever
-    if (!headers['content-length'] && blob.size) {
-      headers['content-length'] = blob.size
-    }
-
-    return res.send(Readable.from(blob.stream()), 200, headers)
-  } catch (e) {
-    error(e)
-  }
-}
+export default serve(app)
