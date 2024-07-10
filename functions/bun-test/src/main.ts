2fb5fea5aa186acecc908ac7856f68faf88510af
Daniil Veremchuk <daniel@verydanny.com>
2024-07-10 15:14:19 -0400

    test with bindings

--- functions/bun-test/src/main.ts
+++ functions/bun-test/src/main.ts
@@ -1,11 +1,12 @@
 /**
- * Using node-21.0 open-runtime
+ * Using bun-1.0 open-runtime
  */
 import { serve } from '@gravlabs/appwrite-hono-adapter-bun'
 import { serveStatic } from '@gravlabs/appwrite-hono-adapter-bun/serveStatic'
+import { AppwriteBindings } from '@gravlabs/appwrite-hono-adapter-bun/types'
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono<{ Bindings: AppwriteBindings }>()
 
 app.get('/static/*', serveStatic({
     root: './'
