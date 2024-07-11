c14e4439d231dcabd4c149d5d5dd9ab9a0a9398f
Daniil Veremchuk <daniel@verydanny.com>
2024-07-10 20:54:43 -0400

    no cache

--- functions/bun-test/src/main.ts
+++ functions/bun-test/src/main.ts
@@ -2,21 +2,12 @@
  * Using bun-1.0 open-runtime
  */
 import { Hono } from 'hono'
-import { cache } from 'hono/cache'
 
 import { serve } from '@gravlabs/appwrite-hono-adapter-bun'
 import { serveStatic } from '@gravlabs/appwrite-hono-adapter-bun/serveStatic'
 import type { AppwriteBindings } from '@gravlabs/appwrite-hono-adapter-bun/types'
 
 const app = new Hono<{ Bindings: AppwriteBindings }>()
-
-app.get(
-    '*',
-    cache({
-        cacheName: 'my-app',
-        cacheControl: 'max-age=3600'
-    })
-)
 
 app.get('/static/*', serveStatic({
     root: './'
